8491f41c272db63a8e616e65dca2f8e2afb11dff
Patrick Vieira Lima <patrickvieira.dev@gmail.com>
2023-04-13 17:00:29 -0300

    Initial commit

---
+++ pizzas.js
@@ -0,0 +1,110 @@
+let pizzaJson = [
+  {
+    id: 1,
+    name: "Mussarela",
+    img: "images/pizza.png",
+    price: 20.19,
+    sizes: ["100g", "530g", "860g"],
+    description:
+      "Pizza de mussarela: Clássica e irresistível, com queijo derretido e molho de tomate fresco.",
+  },
+  {
+    id: 2,
+    name: "Calabresa",
+    img: "images/pizza2.png",
+    price: 18.0,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pizza de calabresa: Saborosa e picante, com fatias de calabresa, cebola e queijo derretido.",
+  },
+  {
+    id: 3,
+    name: "Quatro Queijos",
+    img: "images/pizza3.png",
+    price: 17.45,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pizza quatro queijos: Deliciosamente cremosa, com muçarela, provolone, gorgonzola e parmesão gratinados.",
+  },
+  {
+    id: 4,
+    name: "Americana",
+    img: "images/pizza4.png",
+    price: 19.77,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pizza Americana: Uma explosão de sabores, com pepperoni, cebola, pimentão e muçarela derretida.",
+  },
+  {
+    id: 5,
+    name: "Sorvete",
+    img: "images/pizza5.png",
+    price: 21.43,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pizza Sorvete: Sobremesa inusitada, com sorvete de baunilha, calda de chocolate e morangos frescos.",
+  },
+  {
+    id: 6,
+    name: "Moda da Casa",
+    img: "images/pizza6.png",
+    price: 18.55,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pizza moda da casa: Exótica e deliciosa, com ingredientes secretos e combinação surpreendente.",
+  },
+  {
+    id: 7,
+    name: "Chocolate",
+    img: "images/pizza7.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Pizza de chocolate: Doce e irresistível, com cobertura de chocolate derretido.",
+  },
+  {
+    id: 8,
+    name: "Banana C/ Doce de leite",
+    img: "images/banana.png",
+    price: 21.46,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Banana com doce de leite: Pizza doce com fatias de banana e doce de leite cremoso.",
+  },
+  {
+    id: 9,
+    name: "Chocolate C/ M&M's",
+    img: "images/mm.png",
+    price: 22.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Chocolate com M&M's: Pizza de chocolate com pedaços crocantes de M&M's.",
+  },
+  {
+    id: 10,
+    name: "Chocolate C/ Morango",
+    img: "images/morango.png",
+    price: 23.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Chocolate com morango: Pizza doce de chocolate com morangos frescos fatiados.",
+  },
+  {
+    id: 11,
+    name: "Atum",
+    img: "images/atum.png",
+    price: 32.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Atum: Pizza salgada com atum em conserva, molho de tomate e queijo derretido, criando um sabor delicioso e satisfatório que agrada a maioria dos paladares.",
+  },
+  {
+    id: 12,
+    name: "Brocolis C/ Catupiry",
+    img: "images/brocolis.png",
+    price: 28.36,
+    sizes: ["320g", "530g", "860g"],
+    description:
+      "Brócolis com catupiry: Pizza salgada com brócolis cozido e queijo catupiry cremoso.",
+  },
+];
